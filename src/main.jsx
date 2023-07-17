58b63b10dab86c9a0256190bad1c8f5dd226c1c7
arunya goojar <71588718+arunyagoojar@users.noreply.github.com>
2023-07-17 18:22:19 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,3 +7,17 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>,
 )
+
+// Check that service workers are supported
+if ('serviceWorker' in navigator) {
+  // Use the window load event to keep the page load performant
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js').then(registration => {
+      // Successful registration
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, err => {
+      // Failed registration, service worker won’t be installed
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
